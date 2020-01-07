31d29fee7615e2acc912c142f2e60df8ba443042
Trevor Norris <trev.norris@gmail.com>
2020-01-07 14:46:47 -0700

    Add another function to make things simpler
    Need to figure out a workflow that'll make it easier to generate the end
    results I want from the basic inputs to automatically running the
    orbital simulator and recording its information.

--- tools/REPL_load.js
+++ tools/REPL_load.js
@@ -1,5 +1,5 @@
 const Vector = require('../lib/vector');
-const { orbit_period, orbit_period_days, star_to_period, rotate_main, rotate_second, grshift, grshift_as, darcsec, adiam, sec_to_string, relative_mag } = require('../lib/misc_fns');
+const { orbit_period, orbit_period_days, star_to_period, gen_star, rotate_main, rotate_second, grshift, grshift_as, darcsec, adiam, sec_to_string, relative_mag } = require('../lib/misc_fns');
 const VALS = require('../lib/constants');
 const { G, AU, C } = VALS.CONSTS;
 const { SUN, MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE, STARS } = VALS;
